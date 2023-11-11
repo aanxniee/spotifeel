1c1355dbcb2fbf7339927f9c2478e84b0f28a95f
aanxniee <caishajia@gmail.com>
2023-11-10 23:24:16 -0500

    adjusted mood profiles

--- app/utils/mood_profiles.py
+++ app/utils/mood_profiles.py
@@ -2,49 +2,51 @@
 
 MOOD_PROFILES = {
     'neutral': {
-        'valence': (0.3, 0.5),
-        'energy': (0.3, 0.5),
-        'danceability': (0.3, 0.5),
-        'tempo': (90, 110)
+        'valence': (0.2, 0.6),
+        'energy': (0.2, 0.6),
+        'danceability': (0.2, 0.6),
+        'tempo': (80, 120)
     },
     'calm': {
-        'valence': (0.0, 0.4),
-        'energy': (0.0, 0.4),
-        'danceability': (0.0, 0.4),
-        'tempo': (60, 90)
+        'valence': (0.0, 0.5),
+        'energy': (0.0, 0.5),
+        'danceability': (0.0, 0.5),
+        'tempo': (50, 100)
     },
     'happy': {
-        'valence': (0.5, 1.0),
-        'energy': (0.5, 1.0),
-        'danceability': (0.5, 1.0),
-        'tempo': (100, 200)
+        'valence': (0.4, 1.0),
+        'energy': (0.4, 1.0),
+        'danceability': (0.4, 1.0),
+        'tempo': (90, 200)
     },
     'sad': {
-        'valence': (0.0, 0.3),
-        'energy': (0.0, 0.4),
-        'danceability': (0.0, 0.3),
-        'tempo': (60, 90)
+        'valence': (0.0, 0.4),
+        'energy': (0.0, 0.5),
+        'danceability': (0.0, 0.4),
+        'tempo': (50, 100)
     },
     'angry': {
-        'valence': (0.2, 0.5),
-        'energy': (0.7, 1.0),
-        'danceability': (0.4, 0.6),
-        'tempo': (120, 180)
+        'valence': (0.2, 0.6),
+        'energy': (0.6, 1.0),
+        'danceability': (0.3, 0.7),
+        'tempo': (100, 180)
     },
     'fearful': {
-        'valence': (0.0, 0.4),
-        'energy': (0.4, 0.6),
-        'danceability': (0.3, 0.5),
-        'tempo': (90, 120)
+        'valence': (0.0, 0.5),
+        'energy': (0.3, 0.7),
+        'danceability': (0.2, 0.6),
+        'tempo': (80, 130)
     },
-    'disgust': {'valence': (0.0, 0.3),
-                'energy': (0.4, 0.6),
-                'danceability': (0.2, 0.4),
-                'tempo': (90, 110)
-                },
-    'surprised': {'valence': (0.6, 1.0),
-                  'energy': (0.6, 1.0),
-                  'danceability': (0.5, 0.8),
-                  'tempo': (100, 150)
-                  },
+    'disgust': {
+        'valence': (0.0, 0.4),
+        'energy': (0.3, 0.7),
+        'danceability': (0.1, 0.5),
+        'tempo': (80, 120)
+    },
+    'surprised': {
+        'valence': (0.5, 1.0),
+        'energy': (0.5, 1.0),
+        'danceability': (0.4, 0.9),
+        'tempo': (90, 160)
+    },
 }
