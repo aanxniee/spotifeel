2b512246d99403539f9e3d576d1b04519dc96aa9
aanxniee <caishajia@gmail.com>
2023-11-17 23:00:58 -0500

    added custom mood messages

--- spotifeel-frontend/src/pages/index.tsx
+++ spotifeel-frontend/src/pages/index.tsx
@@ -7,6 +7,37 @@ type PlaylistResponse = {
   uri: string;
 };
 
+interface MoodMessages {
+  [key: string]: string;
+}
+
+const MOOD_MESSAGES: MoodMessages = {
+  neutral: "here's something to keep you balanced.",
+  calm: "relax and unwind with these tunes.",
+  happy: "let's keep the good vibes going!",
+  sad: "it might be rough right now, but cheer up!",
+  angry: "some tracks to help you cool down.",
+  fearful: "music to comfort you in times of fear.",
+  disgust: "shake off the bad vibes with these.",
+  surprised: "surprise! hope these tracks amaze you.",
+};
+
+// Function to get message based on mood
+const getMessageForMood = (mood: string): string => {
+  return MOOD_MESSAGES[mood] || "enjoy these tunes!";
+};
+
+const createEmbedUrl = (uri: string) => {
+  const playlistIdMatch = uri.match(/playlist\/([a-zA-Z0-9]+)/);
+  if (playlistIdMatch && playlistIdMatch[1]) {
+    const playlistId = playlistIdMatch[1];
+    return `https://open.spotify.com/embed/playlist/${playlistId}?utm_source=generator`;
+  } else {
+    console.error("Invalid Spotify URL");
+    return null;
+  }
+};
+
 export default function Home() {
   const [file, setFile] = useState<File | null>(null);
   const [playlist, setPlaylist] = useState<PlaylistResponse | null>(null);
@@ -47,17 +78,6 @@ export default function Home() {
     }
   };
 
-  const createEmbedUrl = (uri: string) => {
-    const playlistIdMatch = uri.match(/playlist\/([a-zA-Z0-9]+)/);
-    if (playlistIdMatch && playlistIdMatch[1]) {
-      const playlistId = playlistIdMatch[1];
-      return `https://open.spotify.com/embed/playlist/${playlistId}?utm_source=generator`;
-    } else {
-      console.error("Invalid Spotify URL");
-      return null;
-    }
-  };
-
   return (
     <div className="flex flex-col justify-center items-center gap-5">
       <h1 className="text-4xl text-neutral-100">Spotifeel</h1>
@@ -76,11 +96,11 @@ export default function Home() {
         <span className="text-neutral-100 loading loading-dots loading-lg"></span>
       )}
       {playlist && (
-        <div className="mt-5 flex flex-col justify-center items-center gap-3 text-neutral-100 font-poppins">
+        <div className="mt-5 flex flex-col justify-center items-center gap-2 text-neutral-100 font-poppins">
           <h2 className="text-xl italic">
             . . . here is the playlist we curated for you . . .
           </h2>
-          <p className="text-xl">Mood: {playlist.result}</p>
+          <p className="text-xl">{getMessageForMood(playlist.result)}</p>
           <iframe
             src={
               playlist && playlist.uri
@@ -88,11 +108,11 @@ export default function Home() {
                 : undefined
             }
             width="100%"
-            height="352"
+            height="600"
             frameBorder="0"
             allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
             loading="lazy"
-            className="rounded-xl"
+            className="rounded-xl my-8"
           ></iframe>
         </div>
       )}
